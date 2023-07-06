a9c8e1e822c8669addf29d3be3dc73b49bced091
James Kabbes <james.kabbes@gmail.com>
2023-07-06 13:30:42 -0500

    adding files to package data

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 from setuptools import setup
 
 if __name__ == '__main__':
-    setup()
+    setup(
+        package_data={'illini_snake': 
+        [ 
+            'static/opponent_logos/*.png',
+            'static/opponent_logos/*.svg',
+            'static/Illinois.png',
+            'static/nom nom nom.wav'
+        ]
+        }
+    )
